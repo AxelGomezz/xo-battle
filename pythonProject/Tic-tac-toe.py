6a626fbd71d63bae806c790f03dbecdc544173be
Axel Gomez <axelrg09@gmail.com>
2024-12-11 02:09:31 -0300

    refactor print_board and add request_movement and do_movement functions

--- pythonProject/Tic-tac-toe.py
+++ pythonProject/Tic-tac-toe.py
@@ -1,10 +1,29 @@
-board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
 def print_board():
+    print("column\n  1 | 2 | 3")
+    i = 1
     for list in board:
+        print(i, end=" ")
         for item in list:
             print(item, end=" | ")
-        print("\n")
+        print("")
+        i+=1
 
+def request_movement():
+    print("\nWhat is your next moviment?")
+
+    row = int(input("Enter row of your selection: "))
+    column = int(input("Enter column of your selection"))
+    return row, column
+
+def do_movement_x(row, column):
+        board[row-1][column-1] = "X"
+
+#MAIN
 print_board()
+row, column = request_movement()
+do_movement_x(row, column)
+print_board()
+
 
