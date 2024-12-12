ed275fb58eb309372ddede8a644e73c90d7b0f43
Axel Gomez <axelrg09@gmail.com>
2024-12-12 17:30:11 -0300

    add handling of exceptions in request_movement function

--- pythonProject/Tic-tac-toe.py
+++ pythonProject/Tic-tac-toe.py
@@ -1,7 +1,7 @@
-board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+board = [["X", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 
 def print_board():
-    print("column\n  1 | 2 | 3")
+    print("  1 | 2 | 3")
     i = 1
     for list in board:
         print(i, end=" ")
@@ -12,13 +12,23 @@ def print_board():
 
 def request_movement():
     print("\nWhat is your next moviment?")
-
-    row = int(input("Enter row of your selection: "))
-    column = int(input("Enter column of your selection"))
-    return row, column
+    while True:
+        try:
+            row = int(input("Enter row of your selection: "))
+            column = int(input("Enter column of your selection: "))
+        except ValueError:
+            print("\nERROR: Invalid value\nPlease enter a number of position in board.")
+        if board[row-1][column-1] == " ":
+            return row, column
+            break
+        else:
+            print("\nThat position in board is not empty\nPlease enter a position empty in board\n")
 
 def do_movement_x(row, column):
-        board[row-1][column-1] = "X"
+    board[row-1][column-1] = "X"
+
+def do_movement_o(row,column):
+    board[row-1][column-1] = "O"
 
 #MAIN
 print_board()
